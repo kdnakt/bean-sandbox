714e48d67001e285521090a35a79a1228128b0c1
kdnakt <a.kid.1985@gmail.com>
2018-05-23 02:24:12 +0900

    initial commit

---
+++ src/main/java/com/kdnakt/spring/bean_sandbox/MySpringApp.java
@@ -0,0 +1,13 @@
+package com.kdnakt.spring.bean_sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class MySpringApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringApp.class, args);
+    }
+
+}
